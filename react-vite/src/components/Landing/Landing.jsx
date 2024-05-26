c589fc419bac8eb04daed9f6c29b6c45ca723c22
southwestmogrown <southwestmogrown@gmail.com>
2024-05-26 12:16:20 -0500

    landing page layout and responsiveness complete

--- react-vite/src/components/Landing/Landing.jsx
+++ react-vite/src/components/Landing/Landing.jsx
@@ -1,6 +1,7 @@
 import { useDispatch, useSelector } from 'react-redux'
 import { Navigate } from 'react-router-dom'
 import { useEffect } from 'react'
+import { Box, Container, Typography, useMediaQuery, useTheme } from '@mui/material'
 
 import LoginFormModal from '../LoginFormModal'
 import OpenModalButton from '../OpenModalButton'
@@ -10,6 +11,11 @@ import { thunkAuthenticate } from '../../redux/session'
 
 function Landing() {
   const dispatch = useDispatch();
+  const theme = useTheme()
+  const sm = useMediaQuery(theme.breakpoints.down('sm'))
+  const md = useMediaQuery(theme.breakpoints.down('md'))
+  const lg = useMediaQuery(theme.breakpoints.down('lg'))
+
   const sessionUser = useSelector(state => state.session.user)
   useEffect(() => {
     dispatch(thunkAuthenticate());
@@ -18,11 +24,98 @@ function Landing() {
   if (sessionUser) return <Navigate to='/home' replace={ true }/>
 
   return (
-    <div className="landing-container">
-
-      <h1>Welcome to ChknTndr!</h1>
-      <h2>A fun new way to find a place to eat with your friends and loved ones.</h2>
-      <div className='btn-container'>
+    <Box
+      sx={{ 
+        bgcolor: 'secondary.main',
+        height: "80%",
+        width: "60%",
+        boxShadow: 20,
+        borderRadius: "25px",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        [theme.breakpoints.down("sm")]: {
+          overflowY: "scroll",
+          overflowX: "none",
+          width: "100%",
+          height: "100%",
+          borderRadius: 0
+        }
+      }}  
+      maxWidth="lg"
+    >
+      <Typography 
+        variant='h1'
+        sx={{ 
+          color: 'primary.text',
+          [theme.breakpoints.down("md")]: {
+            display: "none"
+          },
+          [theme.breakpoints.down("lg")]: {
+            fontSize: "64px"
+          },
+        }}
+      >
+        Welcome to ChknTndr!
+      </Typography>
+      <Box  
+        component="img"
+        src="../../chkntndr-landing.png"
+        alt="ChknTndr landing rooster image"
+        sx={{
+          [theme.breakpoints.down("md")]: {
+            height: "300px"
+          },
+          [theme.breakpoints.down("lg")]: {
+            height: "300px"
+          },
+          [theme.breakpoints.up("lg")]: {
+            height: "350px"
+          },
+        }}
+      />
+      <Typography 
+        variant='h3'
+        sx={{ 
+          color: 'primary.text', 
+          width: "60%",
+          [theme.breakpoints.down("sm")]: {
+            fontSize: "18px",
+            textAlign: "center"
+          },
+          [theme.breakpoints.down("md")]: {
+            fontSize: "22px",
+            textAlign: "center"
+          },
+          [theme.breakpoints.down("lg")]: {
+            fontSize: "28px",
+            textAlign: "center"
+          },
+          [theme.breakpoints.down("xl")]: {
+            fontSize: "28px",
+            textAlign: "center"
+          },
+          [theme.breakpoints.up("xl")]: {
+            fontSize: "30px",
+            textAlign: "center"
+          },
+        }}
+      >
+        A fun new way to find a place to eat with your friends and loved ones.
+      </Typography>
+      <Container
+        sx={{
+          display: "flex",
+          justifyContent: md ? "center" : "space-around",
+          alignItems: "center",
+          width: "50%",
+          marginTop: "2em",
+          [theme.breakpoints.down("md")]: {
+            width: "100%"
+          }
+        }}
+      >
         <OpenModalButton
           buttonText="Log In"
           modalComponent={<LoginFormModal />}
@@ -31,8 +124,8 @@ function Landing() {
           buttonText="Signup"
           modalComponent={<SignupFormModal />}
         />
-      </div>
-    </div>
+      </Container>
+    </Box>
   )
 }
 
--- react-vite/src/components/Navigation/Navigation.jsx
+++ react-vite/src/components/Navigation/Navigation.jsx
@@ -2,6 +2,7 @@ import { NavLink, useNavigate } from "react-router-dom";
 import "./Navigation.css";
 import { useDispatch } from "react-redux";
 import { thunkLogout } from "../../redux/session";
+import { Toolbar, Box, AppBar, Avatar, Container, Button} from "@mui/material";
 
 function Navigation() {
   const navigate = useNavigate()
@@ -14,16 +15,51 @@ function Navigation() {
     navigate('/')
   }
   return (
-    <header className="navbar-container">
-      <div className="logo-container">
-        <NavLink to="/"><img className="logo" src="../../chkntndr-logo.png"/></NavLink>
-      </div>
-      <nav className="navbar">
-        <NavLink className="nav-link" to="/friends">Friends</NavLink>
-        <NavLink className="nav-link" to="/places">Find a new place</NavLink>
-      </nav>
-      <button onClick={onLogout} className="logout-btn">Log Out</button>
-    </header>
+    <Box >
+      <AppBar 
+        className="navbar-container" 
+        position="static" 
+        sx={{ 
+          p: 2,
+          bgcolor: 'secondary.text'
+        }}
+      >
+        <Toolbar>
+          <Container sx={{ display: "flex", justifyContent: "center"}}>
+            <NavLink to="/">
+              <Avatar 
+                alt="Chkn-Logo" 
+                src="../../chkntndr-logo.png"
+                className="logo"
+                sx={{ 
+                  width: 72, 
+                  height: 72,
+                  flexGrow: 1
+                }}
+
+              />
+            </NavLink>
+          </Container>
+          <Container sx={{ display: "flex", justifyContent: "space-around"}}>
+            <NavLink className="nav-link" to="/friends">Friends</NavLink>
+            <NavLink className="nav-link" to="/places">Find a new place</NavLink>
+          </Container>
+          <Container sx={{ display: "flex", justifyContent: "center"}}>
+            <Button onClick={onLogout}>Log Out</Button>
+          </Container>
+        </Toolbar>
+      </AppBar>
+    </Box>
+    // <header className="navbar-container">
+    //   <div className="logo-container">
+    //     <NavLink to="/"><img className="logo" src="../../chkntndr-logo.png"/></NavLink>
+    //   </div>
+    //   <nav className="navbar">
+    //     <NavLink className="nav-link" to="/friends">Friends</NavLink>
+    //     <NavLink className="nav-link" to="/places">Find a new place</NavLink>
+    //   </nav>
+    //   <button onClick={onLogout} className="logout-btn">Log Out</button>
+    // </header>
   );
 }
 
